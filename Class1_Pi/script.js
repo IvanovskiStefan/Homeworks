b600ca1fd255ba3753d264072ab76ff55e85d278
IvanovskiStefan <96203470+IvanovskiStefan@users.noreply.github.com>
2021-12-19 13:28:20 -0800

    Add files via upload

---
+++ Class1_Pi/script.js
@@ -0,0 +1,8 @@
+
+let radius = 20
+//Math.PI go najdov na net deka e funkcija za da ne se pisuva racno Pi :D
+let area = Math.PI*radius*radius
+
+let calculatedArea = "The area of a circle with 20 cm radius is " + area + "cm²"
+
+console.log (calculatedArea )
