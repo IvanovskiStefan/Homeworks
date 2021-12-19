d28535dfcd2481a80e3a69f61fdb464bad074bf9
IvanovskiStefan <96203470+IvanovskiStefan@users.noreply.github.com>
2021-12-19 14:57:07 -0800

    Add files via upload

---
+++ Class1/script.js
@@ -0,0 +1,6 @@
+let numberOfPhones=30
+let priceOfOnePhone = 119.95
+let taxRate = (priceOfOnePhone/100*5)
+let calculatedPrice = taxRate + priceOfOnePhone
+let phoneValue = calculatedPrice*30
+console.log(phoneValue)
