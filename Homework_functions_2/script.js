86373026135f1629c0c84cd5df83ba5c0b0886f2
IvanovskiStefan <96203470+IvanovskiStefan@users.noreply.github.com>
2021-12-28 12:29:49 -0800

    Add files via upload

---
+++ Homework_functions_2/script.js
@@ -0,0 +1,21 @@
+
+function convHumanYears(age) {
+
+    let dogYears = age * 7
+    console.log(dogYears)
+    return dogYears
+
+}
+
+convHumanYears(32);
+
+
+function convDogYears(age) {
+
+    let humanYears = age / 7
+    console.log(humanYears)
+    return humanYears
+
+}
+
+convDogYears(7);
